0060b917df3214a7973e8d05ab54a414adcb4f1c
James Sherry <james.m.sherry@googlemail.com>
2023-04-20 21:11:29 +0100

    next-connect added

--- pages/api/v1/cars/[id].js
+++ pages/api/v1/cars/[id].js
@@ -4,36 +4,39 @@ import {
   getSession,
   // getAccessToken,
 } from "@auth0/nextjs-auth0";
-import nc from "next-connect";
+import { createRouter, expressWrapper } from "next-connect";
+import cors from "cors";
 
 import "../../../../lib/db";
 
-import {
-  updateCar,
-  removeCar,
-} from "@/lib/controllers/car.controller";
+import { updateCar, removeCar } from "@/lib/controllers/car.controller";
 
-const handler = async (req, res) => {
-  try {
-    const session = await getSession(req, res);
-    req.user = session.user;
-    switch (req.method) {
-      case "PUT":
-        return updateCar(req, res);
-      case "DELETE":
-        return removeCar(req, res);
-      default:
-        res.status(400).json({
-          message: `Method ${req.method} not supported for ${new URL(req.url).pathname}`
-        });
+const router = createRouter();
+
+router
+  .use(expressWrapper(cors()))
+  .use(async (req, res, next) => {
+    try {
+      const session = await getSession(req, res);
+      req.user = session.user;
+      return next();
+    } catch (err) {
+      res.status(500).end("MW error!");
     }
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      message: err.message,
-    })
-  }
+  })
+  .put((req, res) => {
+    return updateCar(req, res);
+  })
+  .delete((req, res) => {
+    return deleteCar(req, res);
+  });
 
-}
-
-export default handler;
+export default router.handler({
+  onError: (err, req, res) => {
+    console.error(err.stack);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Page is not found");
+  },
+});
--- pages/api/v1/cars/index.js
+++ pages/api/v1/cars/index.js
@@ -4,36 +4,62 @@ import {
   getSession,
   // getAccessToken,
 } from "@auth0/nextjs-auth0";
-import nc from "next-connect";
+import { createRouter, expressWrapper } from "next-connect";
+import cors from "cors";
 
 import "../../../../lib/db";
 
-import {
-  getCars,
-  addCar,
-} from "@/lib/controllers/car.controller";
+import { getCars, addCar } from "@/lib/controllers/car.controller";
 
-const handler = async (req, res) => {
-  try {
-    const session = await getSession(req, res);
-    req.user = session.user;
-    switch (req.method) {
-      case "GET":
-        return getCars(req, res);
-      case "POST":
-        return addCar(req, res);
-      default:
-        res.status(400).json({
-          message: `Method ${req.method} not supported for ${new URL(req.url).pathname}`
-        });
+const router = createRouter();
+
+router
+  .use(expressWrapper(cors()))
+  .use(async (req, res, next) => {
+    try {
+      const session = await getSession(req, res);
+      req.user = session.user;
+      return next();
+    } catch (err) {
+      res.status(500).end("MW error!");
     }
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      message: err.message,
-    })
-  }
+  })
+  .get((req, res) => {
+    return getCars(req, res);
+  })
+  .post((req, res) => {
+    return addCar(req, res);
+  })
 
-}
+// const handler = async (req, res) => {
+//   try {
+//     const session = await getSession(req, res);
+//     req.user = session.user;
+//     switch (req.method) {
+//       case "GET":
+//         return getCars(req, res);
+//       case "POST":
+//         return addCar(req, res);
+//       default:
+//         res.status(400).json({
+//           message: `Method ${req.method} not supported for ${new URL(req.url).pathname}`
+//         });
+//     }
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).json({
+//       message: err.message,
+//     })
+//   }
 
-export default handler;
+// }
+
+export default router.handler({
+  onError: (err, req, res) => {
+    console.error(err.stack);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Page is not found");
+  },
+});
