d3c9c558c352670c3958bf085d2d5d767ee901c2
James Sherry <james.m.sherry@googlemail.com>
2024-12-05 17:14:16 +0000

    upgraded auth0 to v3.5

--- pages/api/private.js
+++ pages/api/private.js
@@ -8,13 +8,14 @@ import {
 const handler = async (req, res) => {
   try {
     const session = await getSession(req, res);
-    const token = await getAccessToken(req, res);
+    const token = await getAccessToken(req, res); // These throw if not Auth'd
     const user = session.user;
     console.log("session", session);
     console.log("token", token);
     console.log("user", user);
     res.status(200).json({ message: "Welcome to our private data" });
   } catch (err) {
+    console.log(err);
     res.status(401).json({ message: "Unauthorized" });
   }
 };
